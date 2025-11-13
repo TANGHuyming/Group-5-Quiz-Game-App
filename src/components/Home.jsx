92ce0ab7e152ea9cb432ef4b48018f0751157dd4
TANGHuyming <2023277tang@aupp.edu.kh>
2025-11-13 10:18:44 +0700

    homepage 75% done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,64 @@
+import { useState, useEffect } from "react";
+
 export default function Home() {
+    const [username, setUsername] = useState("");
+    const [difficulty, setDifficulty] = useState("easy");
+    const [difficultyText, setDifficultyText] = useState("#33FF00");
+    const [modifier, setModifier] = useState("modifier");
+    const [modifierText, setModifierText] = useState("#33FF00");
+
+    // color maps for options
+    const difficultyColors = {
+        easy: "#33FF00",
+        medium: "#FF8000",
+        hard: "#FF0000",
+    };
+
+    const modifierColors = {
+        nomod: "#1eff00ff",
+        hidden: "#DD00FF",
+        sonicSpeed: "#00FFF2",
+        perfect: "#FF0051",
+    };
+
+    useEffect(() => {
+        switch(difficulty) {
+            case "easy":
+                setDifficultyText(difficultyColors.easy);
+                break;
+            case "medium":
+                setDifficultyText(difficultyColors.medium);
+                break;
+            case "hard":
+                setDifficultyText(difficultyColors.hard);
+                break;
+            default:
+                break;
+        }
+
+        switch(modifier) {
+            case "nomod": 
+                setModifierText(modifierColors.nomod);
+                break;
+            case "hidden":
+                setModifierText(modifierColors.hidden);
+                break;
+            case "sonicSpeed":
+                setModifierText(modifierColors.sonicSpeed);
+                break;
+            case "perfect":
+                setModifierText(modifierColors.perfect);
+                break;
+            default:
+                break;
+        }
+    }, [difficulty, modifier])
 
     const handleSubmit = (event) => {
         event.preventDefault();
         const formData = new FormData(event.target);
-        console.log(Object.fromEntries(formData));
+        const dataObject = Object.entries(formData);
+        setUsername("");
     }
 
     return (
@@ -15,27 +70,31 @@ export default function Home() {
             </div>
 
             <form onSubmit={handleSubmit} className="form-container">
-                <input type="text" required placeholder="Enter your name: " className="input-box" name="playerName"/>
+                <input type="text" required placeholder="Enter your name: " className="input-box" name="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
                 
                 <div className="radio-container">
                     <h2>Choose Your Difficulty: </h2>
 
                     <div className="radio-subcontainer">
                         <div>
-                            <label htmlFor="easy"><div className="solid-circle" style={{backgroundColor: "#33FF00"}}></div> Easy</label>
-                            <input type="radio" required id="easy" name="difficulty" value="easy"/>
+                            <label htmlFor="easy"><div className="solid-circle" style={{backgroundColor: difficultyColors.easy}}></div> Easy</label>
+                            <input type="radio" required id="easy" name="difficulty" value="easy" onClick={()=>setDifficulty("easy")}/>
                         </div>
 
                         <div>
-                            <label htmlFor="medium"><div className="solid-circle" style={{backgroundColor: "#FF8000"}}></div> Medium</label>
-                            <input type="radio" required id="medium" name="difficulty" value="medium"/>
+                            <label htmlFor="medium"><div className="solid-circle" style={{backgroundColor: difficultyColors.medium}}></div> Medium</label>
+                            <input type="radio" required id="medium" name="difficulty" value="medium" onClick={()=>setDifficulty("medium")}/>
                         </div>
 
                         <div>
-                            <label htmlFor="hard"><div className="solid-circle" style={{backgroundColor: "#FF0000"}}></div> Hard</label>
-                            <input type="radio" required id="hard" name="difficulty" value="hard"/>
+                            <label htmlFor="hard"><div className="solid-circle" style={{backgroundColor: difficultyColors.hard}}></div> Hard</label>
+                            <input type="radio" required id="hard" name="difficulty" value="hard" onClick={()=>setDifficulty("hard")}/>
                         </div>
+
                     </div>
+                    <h2>You Chose:
+                        <span style={{color: difficultyText}}> {difficulty}</span>
+                    </h2>
                 </div>
 
                 <div className="radio-container">
@@ -43,20 +102,28 @@ export default function Home() {
 
                     <div className="radio-subcontainer">
                         <div>
+                            <label htmlFor="nomod"><div className="solid-circle" style={{backgroundColor: "#1eff00ff"}}></div> No Mod</label>
+                            <input type="radio" id="nomod" name="modifier" value="nomod" onClick={()=>setModifier("nomod")}/>
+                        </div>
+
+                        <div>
                             <label htmlFor="hidden"><div className="solid-circle" style={{backgroundColor: "#DD00FF"}}></div> Hidden</label>
-                            <input type="radio" id="hidden" name="modifier" value="hidden"/>
+                            <input type="radio" id="hidden" name="modifier" value="hidden" onClick={()=>setModifier("hidden")}/>
                         </div>
 
                         <div>
                             <label htmlFor="sonicSpeed"><div className="solid-circle" style={{backgroundColor: "#00FFF2"}}></div> Sonic Speed</label>
-                            <input type="radio" id="sonicSpeed" name="modifier" value="sonicSpeed"/>
+                            <input type="radio" id="sonicSpeed" name="modifier" value="sonicSpeed" onClick={()=>setModifier("sonicSpeed")}/>
                         </div>
 
                         <div>
                             <label htmlFor="perfect"><div className="solid-circle" style={{backgroundColor: "#FF0051"}}></div> Perfect</label>
-                            <input type="radio" id="perfect" name="modifier" value="perfect"/>
+                            <input type="radio" id="perfect" name="modifier" value="perfect" onClick={()=>setModifier("perfect")}/>
                         </div>
                     </div>
+                    <h2>You Chose: 
+                        <span style={{color: modifierText}}> {modifier}</span>
+                    </h2>
                 </div>
 
                 <button className="submit-button">Start Game!</button>
