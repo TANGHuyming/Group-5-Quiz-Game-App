11ac8d3828a3c2a397cec7cef83f00ae3e283bd5
tkimhong <91110603+tkimhong@users.noreply.github.com>
2025-11-17 02:19:07 +0700

    Add sample leaderboard data with state hook
    Create state to store 12 player objects in an array for now
    Each player object has 3 properties: rank, name, and score
    The data is stored in leaderboardData that we can use later
    12 players is probably enough to make scrolling necessary
    Temporary fake data, hopefully it can come from a real db in the future

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+
 export default function Leaderboard() {
+  // For now this is just sample data
+  // ? Maybe get from this data API or codebase?
+  const [leaderboardData] = useState([
+    { rank: 1, name: "José Ramírez", score: 283 },
+    { rank: 2, name: "Steven Kwan", score: 272 },
+    { rank: 3, name: "Kyle Manzardo", score: 234 },
+    { rank: 4, name: "Brayan Rocchio", score: 233 },
+    { rank: 5, name: "Ángel Martínez", score: 224 },
+    { rank: 6, name: "George Valera", score: 220 },
+    { rank: 7, name: "Gabriel Arias", score: 220 },
+    { rank: 8, name: "C. J. Kayfus", score: 220 },
+    { rank: 9, name: "Daniel Schneemann", score: 206 },
+    { rank: 10, name: "Johnathan Rodríguez", score: 197 },
+    { rank: 11, name: "Bo Naylor", score: 195 },
+    { rank: 12, name: "Jhonkensy Noel", score: 162 },
+  ]);
+
   return (
     <div className="leaderboard-container">
       <h1 className="title">LEADERBOARD</h1>
