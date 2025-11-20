99d0acd0ccdbe57c562a8fc530bdbfcdd9ad0c1c
TANGHuyming <2023277tang@aupp.edu.kh>
2025-11-20 09:02:44 +0700

    About us page 75% done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,8 @@
 import AboutCard from './AboutCard';
 import { useState } from 'react';
+import huymingImage from '../Images/Huyming.jpg';
+import KimseanImage from '../Images/Kimsean.jpg';
+import KimhongImage from '../Images/Tiangkimhong.jpg';
 
 export default function About() {
     const [name, setName] = useState('');
@@ -24,14 +27,22 @@ export default function About() {
         setMessage('');
     }
 
+    const developers = [
+        {image: huymingImage, name: "Huyming", description: "Leader of Triva Bonanza"},
+        {image: KimseanImage, name: "Kimsean", description: "Member of Triva Bonanza"},
+        {image: KimhongImage, name: "TiangKimhong", description: "Member of Triva Bonanza"},
+    ]
+
     return(
         <div className="about-container" style={aboutContainerStyling}>
             <div className="about-first-half">
                 <h2>Developers of Type or Perish!</h2>
                 <div className="about-card-container">
-                    <AboutCard image="./" name="Tang Huyming" description="Team Leader" />
-                    <AboutCard image="./" name="Kimsean" description="Team Member"/>
-                    <AboutCard image="./" name="Tiangkimhong Hoeu" description="Team Member" />
+                    {
+                        developers.map((developer) => {
+                            return <AboutCard image={developer.image} name={developer.name} description={developer.description} />
+                        })
+                    }
                 </div>
             </div>
         
@@ -39,11 +50,11 @@ export default function About() {
                 <h2>Contact Us/Bug Report</h2>
                 <div className="contact-us-container">
                     <form onSubmit={handleSubmit} className="form-container">
-                        <input type="text" className="input-box" style={inputBox} name="name" placeholder="Your name..." value={name} required onClick={(e) => setName(e.target.value)}/>
-                        <input type="email" className="input-box" style={inputBox} name="email" placeholder="Your email..." value={email} required onClick={(e) => setEmail(e.target.value)}/>
-                        <input type="text" className="input-box" style={inputBox} name="subject" placeholder="Subject..." value={subject} required onClick={(e) => setSubject(e.target.value)}/>
-                        <textarea name="message" className="input-box" placeholder="Message..." value={message} required rows="5" cols="50" onClick={(e) => setMessage(e.target.value)}></textarea>
-                        <button className="submit-button">Send Message</button>
+                        <input type="text" className="input-box" style={inputBox} name="name" placeholder="Your name..." value={name} required onChange={(e) => setName(e.target.value)}/>
+                        <input type="email" className="input-box" style={inputBox} name="email" placeholder="Your email..." value={email} required onChange={(e) => setEmail(e.target.value)}/>
+                        <input type="text" className="input-box" style={inputBox} name="subject" placeholder="Subject..." value={subject} required onChange={(e) => setSubject(e.target.value)}/>
+                        <textarea name="message" className="input-box" style={{resize: "none"}} placeholder="Message..." value={message} required rows="5" cols="50" onChange={(e) => setMessage(e.target.value)}></textarea>
+                        <button type="submit" className="submit-button">Send Message</button>
                     </form>
                 </div>
             </div>
