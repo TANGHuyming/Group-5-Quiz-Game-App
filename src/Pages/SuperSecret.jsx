cef73774b5310577c81f2a5940b3e45350ce04b4
tkimhong <91110603+tkimhong@users.noreply.github.com>
2025-11-23 16:43:40 +0700

    Add state variables
    Import useState
    Add 6 state variables: difficulty + question + 1 correct + 3 wrong answers
    Add difficultyColors object which is same as Home page

--- src/Pages/SuperSecret.jsx
+++ src/Pages/SuperSecret.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react";
+
 export default function SuperSecret() {
+  // States for all form fields
+  const [difficulty, setDifficulty] = useState("");
+  const [question, setQuestion] = useState("");
+  const [correctAnswer, setCorrectAnswer] = useState("");
+  const [wrongAnswer1, setWrongAnswer1] = useState("");
+  const [wrongAnswer2, setWrongAnswer2] = useState("");
+  const [wrongAnswer3, setWrongAnswer3] = useState("");
+
+  // Colors for difficulty (taken from Home page)
+  const difficultyColors = {
+    easy: "#33FF00",
+    medium: "#FF8000",
+    hard: "#FF0000",
+  };
+
   return (
     <>
       <h1 className="title">Add Your Own Question!</h1>
